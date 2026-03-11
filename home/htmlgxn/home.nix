024227d29577ce2dffc17f220bc1d4e52cfc67d6
htmlgxn <htmlgxn@pm.me>
2026-03-11 03:45:28 -0300

    configure bash via home-manager

--- home/htmlgxn/home.nix
+++ home/htmlgxn/home.nix
@@ -11,7 +11,49 @@
     fd
   ];
 
-  programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+
+    shellAliases = {
+      c = "clear";
+      h = "history";
+      la = "ls -a";
+      lsa = "ls -a";
+      ll = "ls -la";
+      lsla = "ls -la";
+      lsal = "ls -la";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+      grep = "grep --color=auto";
+      egrep = "egrep --color=auto";
+      fgrep = "fgrep --color=auto";
+      edit = "nvim";
+      e = "nvim";
+      ga = "git add .";
+      gs = "git status";
+      gc = "git commit";
+      gp = "git push";
+      calc = "fend";
+      cdc = "cd ~/nixos-config";
+      ef = "nvim ~/nixos-config/flake.nix";
+      eh = "nvim ~/nixos-config/home/htmlgxn/home.nix";
+      econfn = "nvim ~/nixos-config/hosts/nixos-vm/configuration.nix"; 
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-config/.#nixos-vm";
+    };
+
+    sessionVariables = {
+      EDITOR = "nvim";
+      VISUAL = "nvim";
+    };
+
+    bashrcExtra = ''
+      # manually add to .bashrc here
+    '';
+
+    profileExtra = ''
+      # manually add to .bash_profile here
+    '';
+  };
 
   home.stateVersion = "25.11";
 }
