051333ba457fd826ee69c892f8a699e9c9bebfdf
htmlgxn <htmlgxn@pm.me>
2026-03-11 02:45:18 -0300

    fixed syntax error

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-	home-manager.nixosModules.homeManager
+	home-manager.nixosModules.home-manager
 	{
 	  home-manager.useGlobalPkgs = true;
 	  home-manager.useUserPackages = true;
