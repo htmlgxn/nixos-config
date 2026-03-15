7092be7c0febc6a0295c638d04919201e046736f
htmlgxn <htmlgxn@pm.me>
2026-03-15 02:18:01 -0300

    added new cd aliases for mounted drives

--- home/gars/home.nix
+++ home/gars/home.nix
@@ -54,9 +54,12 @@
       eswayc = "nvim ~/nixos-config/home/gars/dots/sway/config";
 
       # ── Misc navigation ─────────────────────────────────────────────
-      cdd = "cd ~/dev";
-      cdp = "cd ~/dev/projects";
-      cdc = "cd ~/nixos-config/home/gars/dots";
+      cdd    = "cd ~/dev";
+      cdp    = "cd ~/dev/projects";
+      cdc    = "cd ~/nixos-config/home/gars/dots";
+      cdarch = "cd /mnt/archive";
+      cdsea  = "cd /mnt/seagate6";
+      cdback = "cd /mnt/backup";
 
       # ── Rebuild: boreal ───────────────────────────────────────────────
       # nrs   — sway (production)
