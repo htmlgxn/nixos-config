c09618bc5f329faf214b96de4c5da5844bb0f78a
htmlgxn <htmlgxn@pm.me>
2026-03-15 19:26:57 -0300

    qbittorrent theming config via nixos - pass 4

--- modules/home/qbittorrent.nix
+++ modules/home/qbittorrent.nix
@@ -14,25 +14,22 @@ in {
   home.file.".config/qBittorrent/qBittorrent.qbt".source =
     config.lib.file.mkOutOfStoreSymlink themePath;
 
-  home.activation.qbittorrentTheme = pkgs.writeShellApplication {
-    name = "qbittorrent-theme";
-    text = ''
-      if [ -f "${configPath}" ]; then
-        # Remove existing theme settings if present
-        sed -i '/^General\\UseCustomUITheme=/d' "${configPath}"
-        sed -i '/^General\\CustomUIThemePath=/d' "${configPath}"
-        
-        # Add theme settings after [Preferences] line
-        awk -v path="${themePath}" '
-          /^\[Preferences\]/ {
-            print
-            print "General\\UseCustomUITheme=true"
-            print "General\\CustomUIThemePath=" path
-            next
-          }
-          { print }
-        ' "${configPath}" > "${configPath}.tmp" && mv "${configPath}.tmp" "${configPath}"
-      fi
-    '';
-  };
+  home.activation.qbittorrentTheme = ''
+    if [ -f "${configPath}" ]; then
+      # Remove existing theme settings if present
+      sed -i '/^General\\UseCustomUITheme=/d' "${configPath}"
+      sed -i '/^General\\CustomUIThemePath=/d' "${configPath}"
+      
+      # Add theme settings after [Preferences] line
+      ${pkgs.gawk}/bin/awk -v path="${themePath}" '
+        /^\[Preferences\]/ {
+          print
+          print "General\\UseCustomUITheme=true"
+          print "General\\CustomUIThemePath=" path
+          next
+        }
+        { print }
+      ' "${configPath}" > "${configPath}.tmp" && mv "${configPath}.tmp" "${configPath}"
+    fi
+  '';
 }
