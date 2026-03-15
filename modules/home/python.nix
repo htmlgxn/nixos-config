a00f3ad2f832bb717976295d4a4ffba72fff6100
htmlgxn <htmlgxn@pm.me>
2026-03-15 18:57:18 -0300

    test deps with variables idek

--- modules/home/python.nix
+++ modules/home/python.nix
@@ -6,6 +6,11 @@
     python314
     uv # Python package/toolchain manager
     stdenv.cc.cc.lib # Runtime dependency for native Python packages (scrapling, etc.)
-    playwright-driver
+    playwright-driver # Playwright with NixOS-compatible browsers
   ];
+
+  # Set PLAYWRIGHT_BROWSERS_PATH to use nixpkgs-provided browsers
+  home.sessionVariables = {
+    PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+  };
 }
