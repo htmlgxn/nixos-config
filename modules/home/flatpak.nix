bb3034a754a43a98fc6d726908f4a5d213bc82bc
htmlgxn <htmlgxn@pm.me>
2026-03-16 04:13:46 -0300

    reformat (and pass 30 )

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -4,25 +4,26 @@
 # User-level Flatpak configuration.
 # Adds Flathub remote and installs Flatpak applications via Home Manager.
 #
-{ config, pkgs, lib, ... }:
-
-let
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
   flatpakPackages = import ./flatpak/packages.nix;
-
 in {
   xdg.systemDirs.data = [
     "${config.home.homeDirectory}/.local/share/flatpak/exports/share"
   ];
 
-  home.activation.installFlatpaks =
-    lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists --user \
-        flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+  home.activation.installFlatpaks = lib.hm.dag.entryAfter ["writeBoundary" "linkGeneration"] ''
+    ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists --user \
+      flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-      for app in ${lib.concatStringsSep " " flatpakPackages}; do
-        echo "flatpak: (re)installing $app..."
-        ${pkgs.flatpak}/bin/flatpak install --user --noninteractive --assumeyes \
-          flathub "$app"
-      done
-    '';
+    for app in ${lib.concatStringsSep " " flatpakPackages}; do
+      echo "flatpak: (re)installing $app..."
+      ${pkgs.flatpak}/bin/flatpak install --user --noninteractive --assumeyes \
+        flathub "$app"
+    done
+  '';
 }
