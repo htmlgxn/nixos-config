901998fdb7e21e35c8e806b0c5f9411b337c5329
htmlgxn <htmlgxn@pm.me>
2026-03-20 02:16:08 -0300

    housekeeping p2-2

--- modules/system/flatpak.nix
+++ modules/system/flatpak.nix
@@ -12,12 +12,6 @@
 }: {
   services.flatpak = {
     enable = true;
-    remotes = {
-      "flathub" = {
-        url = "https://dl.flathub.org/repo/flathub.flatpakrepo";
-        install = lib.mkDefault true;
-      };
-    };
   };
 
   # Enable Flatpak support in NixOS
