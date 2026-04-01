5a293f774da9c34d3f17fda0443c307b1f24ca89
htmlgxn <htmlgxn@pm.me>
2026-04-01 14:19:04 -0300

    added binary caches - let's see

--- modules/system/defaults.nix
+++ modules/system/defaults.nix
@@ -2,7 +2,19 @@
 {...}: {
   time.timeZone = "America/Halifax";
   i18n.defaultLocale = "en_CA.UTF-8";
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
   nix.gc = {
     automatic = true;
     dates = "weekly";
