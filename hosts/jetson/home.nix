7aefa2d240a6af34790b4b9ba2e8078dec366053
htmlgxn <htmlgxn@pm.me>
2026-04-08 04:43:31 -0300

    update jetson path

--- hosts/jetson/home.nix
+++ hosts/jetson/home.nix
@@ -3,6 +3,7 @@ _: {
   home.sessionVariables = {
     CUDA_PATH = "/usr/local/cuda";
     LD_LIBRARY_PATH = "/usr/local/cuda/lib64:/usr/lib/aarch64-linux-gnu";
+    PATH = "/usr/local/cuda/bin:$PATH";
   };
 
   programs.bash.shellAliases.nrs = "nh home switch . -c jetson";
